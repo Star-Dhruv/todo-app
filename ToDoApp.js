9879fd139ce9e5495112f267be4b9eec8d24a084
PankajKharadi <pkcreater45@gmail.com>
2025-07-31 15:45:55 +0530

    I add some css and full js for dynamic add task

--- ToDoApp.js
+++ ToDoApp.js
@@ -2,20 +2,42 @@
 
 function Add() {
 
-
+     // Add Task
     var Ptag = document.createElement("p");
-
-    var BDelete = document.createElement("button");
-    BDelete.textContent = "delete";
-    BDelete.classList.add('Task');
     var text = document.getElementById("AddTask").value;
     Ptag.textContent = text;
-    console.log(text);
-
-
     Ptag.classList.add('Task');
-    // document.getElementsByClassName("Task").setAttribute("type", "checkbox"); 
-    const element = document.getElementById("DisplayTask");
-    element.appendChild(Ptag);
+    console.log(Ptag);
+    Ptag.setAttribute("type", "checkbox");
+     
+    // Add check button
+    var input = document.createElement("input");
+      input.setAttribute("type", "checkbox");
+    
+    // Add Div
+    var div = document.createElement("div");
+    // Add Div another div for button float left
+    var BDiv = document.createElement("div");
+    BDiv.classList.add('btnSide');
 
+    // Add edit button
+    var BEdit = document.createElement("button");
+    BEdit.textContent = "edit";
+    BEdit.classList.add('BEdit');
+
+     // Add Delete button
+     var BDelete = document.createElement("button");
+     BDelete.textContent = "delete";
+     BDelete.classList.add('BDelete');
+   
+    const element = document.getElementById("DisplayTask");
+    div.appendChild(input);
+    div.appendChild(Ptag);
+    BDiv.appendChild(BEdit);
+    BDiv.appendChild(BDelete);
+    div.appendChild(BDiv);
+    element.appendChild(div);
+
+
+   
 }
